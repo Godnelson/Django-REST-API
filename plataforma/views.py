8fede5d4baca5b94815e8067d94c8acc475c01ae
Godnelson <luizgatohd@gmail.com>
2022-03-23 23:35:00 -0300

    finish_UsariosAPI

--- plataforma/views.py
+++ plataforma/views.py
@@ -121,6 +121,7 @@ def UsuariosViewSet(request, pk=''):
                 serializer.save()
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
     else:
         try:
             queryset = Usuarios.objects.get(id=pk)
@@ -129,3 +130,12 @@ def UsuariosViewSet(request, pk=''):
         if request.method == 'GET':
             serializer = UsuariosSerializer(queryset)
             return Response(serializer.data, status=status.HTTP_200_OK)
+        elif request.method == 'PUT':
+            serializer = UsuariosSerializer(queryset, data = request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        elif request.method == 'DELETE':
+            queryset.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
